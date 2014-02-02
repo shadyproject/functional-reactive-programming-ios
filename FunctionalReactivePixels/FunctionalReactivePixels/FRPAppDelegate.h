809e8fcc8daf33bd6d699ddb662153fb8d5dbbed
Shady <shady@shadyproject.net>
2014-02-01 21:24:56 -0800

    Create project

---
+++ FunctionalReactivePixels/FunctionalReactivePixels/FRPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FRPAppDelegate.h
+//  FunctionalReactivePixels
+//
+//  Created by Christopher Martin on 2/1/14.
+//  Copyright (c) 2014 shadyproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FunctionalReactivePixels/FunctionalReactivePixels/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FunctionalReactivePixels
+//
+//  Created by Christopher Martin on 2/1/14.
+//  Copyright (c) 2014 shadyproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FRPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FRPAppDelegate class]));
+    }
+}
