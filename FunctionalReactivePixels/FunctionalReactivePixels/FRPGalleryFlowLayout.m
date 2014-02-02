146c0aeb95c8b47a4f0265a022835f7e03611848
Shady <shady@shadyproject.net>
2014-02-01 21:41:16 -0800

    Implement flow layout

--- FunctionalReactivePixels/FunctionalReactivePixels/FRPGalleryFlowLayout.m
+++ FunctionalReactivePixels/FunctionalReactivePixels/FRPGalleryFlowLayout.m
@@ -10,4 +10,16 @@
 
 @implementation FRPGalleryFlowLayout
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.itemSize = CGSizeMake(145, 145);
+        self.minimumInteritemSpacing = 10;
+        self.minimumLineSpacing = 10;
+        self.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10);
+    }
+    
+    return self;
+}
+
 @end
