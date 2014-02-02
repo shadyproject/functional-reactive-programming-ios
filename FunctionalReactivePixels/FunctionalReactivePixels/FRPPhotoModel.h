1f4351a8fbce23f87588eb47c4fe73a6d7fe351c
Shady <shady@shadyproject.net>
2014-02-01 22:11:04 -0800

    populate model

--- FunctionalReactivePixels/FunctionalReactivePixels/FRPPhotoModel.h
+++ FunctionalReactivePixels/FunctionalReactivePixels/FRPPhotoModel.h
@@ -9,5 +9,12 @@
 #import <Foundation/Foundation.h>
 
 @interface FRPPhotoModel : NSObject
-
+@property (nonatomic, strong) NSString *photoName;
+@property (nonatomic, strong) NSNumber *identifier;
+@property (nonatomic, strong) NSString *photographerName;
+@property (nonatomic, strong) NSNumber *rating;
+@property (nonatomic, strong) NSString *thumbnailUrl;
+@property (nonatomic, strong) NSData *thumbnailData;
+@property (nonatomic, strong) NSString *fullsizedUrl;
+@property (nonatomic, strong) NSDictionary *fullsizedData;
 @end
