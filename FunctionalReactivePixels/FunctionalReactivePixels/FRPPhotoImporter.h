e479c4c94065910513a5030e4edfb4c169dcca84
Shady <shady@shadyproject.net>
2014-02-01 22:50:14 -0800

    create importer

---
+++ FunctionalReactivePixels/FunctionalReactivePixels/FRPPhotoImporter.h
@@ -0,0 +1,13 @@
+//
+//  FRPPhotoImporter.h
+//  FunctionalReactivePixels
+//
+//  Created by Christopher Martin on 2/1/14.
+//  Copyright (c) 2014 shadyproject. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FRPPhotoImporter : NSObject
+
+@end
---
+++ FunctionalReactivePixels/FunctionalReactivePixels/FRPPhotoImporter.m
@@ -0,0 +1,13 @@
+//
+//  FRPPhotoImporter.m
+//  FunctionalReactivePixels
+//
+//  Created by Christopher Martin on 2/1/14.
+//  Copyright (c) 2014 shadyproject. All rights reserved.
+//
+
+#import "FRPPhotoImporter.h"
+
+@implementation FRPPhotoImporter
+
+@end
