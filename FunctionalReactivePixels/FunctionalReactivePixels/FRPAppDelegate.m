7f27f238fb7a96e0e28ace37faa5b73e72a127cf
Shady <shady@shadyproject.net>
2014-02-01 22:07:59 -0800

    set up API, add stuff to pch

--- FunctionalReactivePixels/FunctionalReactivePixels/FRPAppDelegate.m
+++ FunctionalReactivePixels/FunctionalReactivePixels/FRPAppDelegate.m
@@ -9,10 +9,20 @@
 #import "FRPAppDelegate.h"
 #import "FRPGalleryViewController.h"
 
+@interface FRPAppDelegate ()
+@property (nonatomic, strong) PXAPIHelper *apiHelper;
+@end
+
+
 @implementation FRPAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
+    self.apiHelper = [[PXAPIHelper alloc] initWithHost:nil
+                                           consumerKey:@"DC2To2BS0ic1ChKDK15d44M42YHf9gbUJ"
+                                        consumerSecret:@"i8WL4chWoZ4kw9fh3jzHK7XzTer1y5tUNvsTFNnB"];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     FRPGalleryViewController *root = [[FRPGalleryViewController alloc] init];
