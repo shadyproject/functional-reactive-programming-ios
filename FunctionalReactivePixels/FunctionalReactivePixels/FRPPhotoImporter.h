437cb3283aa23c3520b25be6367fe401987d22e0
Shady <shady@shadyproject.net>
2014-02-01 22:50:38 -0800

    implement importer

--- FunctionalReactivePixels/FunctionalReactivePixels/FRPPhotoImporter.h
+++ FunctionalReactivePixels/FunctionalReactivePixels/FRPPhotoImporter.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface FRPPhotoImporter : NSObject
-
++ (RACSignal*)importPhotos;
 @end
--- FunctionalReactivePixels/FunctionalReactivePixels/FRPPhotoImporter.m
+++ FunctionalReactivePixels/FunctionalReactivePixels/FRPPhotoImporter.m
@@ -7,7 +7,72 @@
 //
 
 #import "FRPPhotoImporter.h"
+#import "FRPPhotoModel.h"
 
 @implementation FRPPhotoImporter
++(NSURLRequest*)populateUrlRequest{
+    return [AppDelegate.apiHelper urlRequestForPhotoFeature:PXAPIHelperPhotoFeaturePopular
+                                          resultsPerPage:100 page:0
+                                              photoSizes:PXPhotoModelSizeThumbnail
+                                               sortOrder:PXAPIHelperSortOrderRating
+                                                  except:PXPhotoModelCategoryNude];
+}
+
++(void)configurePhotoModel:(FRPPhotoModel*)photoModel withDictionary:(NSDictionary*)dictionary{
+    photoModel.photoName = dictionary[@"name"];
+    photoModel.identifier = dictionary[@"id"];
+    photoModel.photographerName = dictionary[@"user"];
+    photoModel.rating = dictionary[@"rating"];
+    photoModel.thumbnailUrl = [self urlForImageSize:3 inArray:dictionary[@"images"]];
+    
+    if (dictionary[@"comments_count"]) {
+        photoModel.fullsizedUrl = [self urlForImageSize:4 inArray:dictionary[@"images"]];
+    }
+}
+
++(NSString*)urlForImageSize:(NSInteger)size inArray:(NSArray*)array{
+    return [[[[[array rac_sequence] filter:^BOOL(NSDictionary *value) {
+        return [value[@"size"] integerValue] == size;
+    }] map:^id(id value) {
+        return value[@"url"];
+    }] array] firstObject];
+}
+
++(void)downloadThumbnailForPhotoModel:(FRPPhotoModel*)photoModel{
+    NSAssert(photoModel.thumbnailUrl, @"Thumbnail URL must not be nil");
+    
+    NSURLRequest *req = [NSURLRequest requestWithURL:[NSURL URLWithString:photoModel.thumbnailUrl]];
+    
+    [NSURLConnection sendAsynchronousRequest:req queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                               photoModel.thumbnailData = data;
+                           }];
+}
+
++(RACReplaySubject*)importPhotos{
+    RACReplaySubject *subject = [RACReplaySubject subject];
+    
+    NSURLRequest *req = [self populateUrlRequest];
+    [NSURLConnection sendAsynchronousRequest:req queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        if (data) {
+            id results = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            
+            [subject sendNext:[[[results[@"photos"] rac_sequence] map:^id(NSDictionary *photoDictionary) {
+                FRPPhotoModel *model = [FRPPhotoModel new];
+                [self configurePhotoModel:model withDictionary:photoDictionary];
+                [self downloadThumbnailForPhotoModel:model];
+                return model;
+            }] array]];
+            [subject sendCompleted];
+        } else {
+            [subject sendError:connectionError];
+        }
+    }];
+    
+    return subject;
+}
+
+
 
 @end
