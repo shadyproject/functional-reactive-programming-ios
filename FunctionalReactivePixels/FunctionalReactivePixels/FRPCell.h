c1a6c574a990fa8e028acfdb87f4f89eb658ca12
Shady <shady@shadyproject.net>
2014-02-02 17:13:54 -0800

    create cell for displaying images

---
+++ FunctionalReactivePixels/FunctionalReactivePixels/FRPCell.h
@@ -0,0 +1,13 @@
+//
+//  FRPCell.h
+//  FunctionalReactivePixels
+//
+//  Created by Christopher Martin on 2/1/14.
+//  Copyright (c) 2014 shadyproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRPCell : UICollectionViewCell
+
+@end
